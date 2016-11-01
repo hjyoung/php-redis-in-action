eedecab5452db55ec336e073dab5946b401e1c6e
root <root@dsfy2011.(none)>
2016-11-01 13:07:43 -0400

    init

---
+++ lib/phpRedis/Client.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace phpRedis;
+
+class Client
+{
+	private $redis;
+
+	public function __construct($config)
+	{
+		$redis = new Redis();
+
+		if($redis->connect($config['host'],$config['port'],$config['timeout']))
+		{
+			$this->redis = $redis;
+			return $this->redis;
+		}
+		else
+		{
+			throw new RedisException("Couldn't connect to host");
+		}
+	}	
+
+	public function close()
+	{
+		$this->redis->close();
+	}
+}
